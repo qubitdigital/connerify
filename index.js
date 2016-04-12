95eb8b439462ff9fa7402c69e0851a7028b60d82
Jim O'Brien <jimmed@users.noreply.github.com>
2016-04-12 18:19:31 +0100

    Necessary over-engineering
    When a file looks too simple, I twitch violently and refactor it to be more complex. Here's my take on this module.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var replacements = [
+  [/(s)([^sh])/gi, '$1h$2'],
+  [/(s)$/gi, '$1h'],
+  [/(x)([^\ss])/gi, '$1sh$2'],
+  [/ce/gi, 'sh']
+]
+
+function reduscher (word, replacement) {
+  return String.prototype.replace.apply(word, replacement)
+}
+
 module.exports = function connerify (word) {
-  return word
-    .replace(/(s)([^sh])/gi, '$1h$2')
-    .replace(/(s)$/gi, '$1h')
-    .replace(/(x)([^\ss])/gi, '$1sh$2')
-    .replace(/ce/gi, 'sh')
+  return replacements.reduce(reduscher, word)
 }
